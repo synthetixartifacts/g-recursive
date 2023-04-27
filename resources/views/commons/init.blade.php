27e2002daa356309609f74d32a975b2904306537
Tom <tommy.thierry>
2023-04-27 17:32:26 -0400

    add share picture

--- resources/views/commons/init.blade.php
+++ resources/views/commons/init.blade.php
@@ -2,6 +2,6 @@
 
 // Construct useful variables
 $projectVersion = '0.1.0';
-$imageShare     = Vite::asset('resources/images/logo/logo_share.jpg');
+$imageShare     = Vite::asset('/resources/images/misc/av.jpg');
 
 ?>@include('commons.base')
