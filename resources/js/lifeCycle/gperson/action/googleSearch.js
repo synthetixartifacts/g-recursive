bdcf4eb111ab2958d1be895765def5afa389a323
Tom <tommy.thierry>
2023-04-28 18:12:57 -0400

    search

--- resources/js/lifeCycle/gperson/action/googleSearch.js
+++ resources/js/lifeCycle/gperson/action/googleSearch.js
@@ -50,7 +50,7 @@ class ActionGoogleSearch extends ActionBase {
             self.person.executeActions([{
                 action : 'talkToAI',
                 name   : self.person.getName(),
-                message: 'your search results: ' + JSON.stringify(items) + ' Do not search the same thing twice.'
+                message: 'You search for "' + infos.query + '". These are the search results: ' + JSON.stringify(response)
             }]);
         } catch (error) {
           console.error("Error fetching search results:", error);
@@ -70,7 +70,7 @@ class ActionGoogleSearch extends ActionBase {
                 self.person.executeActions([{
                     action : 'talkToAI',
                     name   : self.person.getName(),
-                    message: 'your search results: ' + JSON.stringify(response)
+                    message: 'You search for "' + infos.query + '". These are the search results: ' + JSON.stringify(response)
                 }]);
             },
             error: function(error) {
